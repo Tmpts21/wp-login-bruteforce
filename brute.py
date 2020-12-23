499155743d55e9fc22a1caa2110c4bb2f119dc10
Tmpts21 <60459436+Tmpts21@users.noreply.github.com>
2020-12-24 00:21:57 +0800

    first commit
    uploading the script

---
+++ brute.py
@@ -0,0 +1,60 @@
+
+import itertools 
+import string 
+import sys  
+import requests 
+import argparse   
+
+def brute( username , password , url)  :    
+    #url 
+        
+
+    PARAMS = { 
+              'username': username, 
+              'password' : password  
+             }   
+    print(url)
+
+    print(PARAMS)
+
+    #r = requests.get(url = URL, params = PARAMS) 
+      
+    #data = r.json() 
+
+    #print(data)
+ 
+def main () :   
+
+    #parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-u", "--username", help="valid username" )
+    parser.add_argument("-url", "--url", help="url to attack  ")
+     
+    args = parser.parse_args()  
+
+    #load the password files  
+    with open("passwords.txt") as passwords:
+        for password in passwords:  
+
+            #valid username
+            username = args.username   
+
+            password = password.rstrip("\n" )  
+
+            url = args.url   
+
+            #brute force the login 
+            brute( username , password  , url )
+
+if __name__ == "__main__":
+    main()  
+
+
+
+        
+
+
+
+
+        
+
